e8cd14db5168ec26a17162f6fe375e684a3b06d7
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-28 22:31:32 +0600

    our services section 50% done

---
+++ src/Pages/Servece-Pages/Banner/OurServices.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const OurServices = () => {
+    return (
+        <>
+            <section className='text-center py-36 space-y-3'>
+                <div className='mx-auto bg-base-200 inline p-3 text-primary font-semibold'>
+                    What We Do
+                </div>
+                <h3 className='font-semibold text-4xl text-[#040741]'>Our Services</h3>
+                <p className='text-center md:px-44'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam totam deserunt sapiente deleniti culpa laboriosam provident labore, molestiae dolorem possimus nostrum, sint consequuntur dicta aliquid sit praesentium? Ratione explicabo doloribus reiciendis animi, officia ullam voluptatem.
+                </p>
+            </section>
+        </>
+    );
+};
+
+export default OurServices;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../Pages/Navbar/Navbar';
 import Banner from '../Pages/Servece-Pages/Banner/Banner';
+import OurServices from '../Pages/Servece-Pages/Banner/OurServices';
 
 const Services = () => {
     return (
         <>
             <Navbar />
             <Banner />
+            <OurServices />
         </>
     );
 };
