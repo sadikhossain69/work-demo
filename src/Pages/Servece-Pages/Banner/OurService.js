633c098e399a9aa902fc1ded4a4614f41de222a3
sadikhossain69 <sadikhossain433@gmail.com>
2022-09-28 23:32:05 +0600

    cards done

--- src/Pages/Servece-Pages/Banner/OurService.js
+++ src/Pages/Servece-Pages/Banner/OurService.js
@@ -1,41 +1,139 @@
 import React from 'react';
+import { BiCodeBlock } from 'react-icons/bi'
+import { MdOutlineBusinessCenter } from 'react-icons/md'
+import { FaProductHunt } from 'react-icons/fa'
+import { SiAntdesign } from 'react-icons/si'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import { IoShareSocialSharp } from 'react-icons/io5'
+import { BsSearch } from 'react-icons/bs'
+import { GrMonitor } from 'react-icons/gr'
 
 const OurService = () => {
     return (
         <>
             <div className="card bg-base-100 shadow-xl w-60">
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
-                </div>
-            </div>
-            <div className="card bg-base-100 shadow-xl w-60">
-                <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <BiCodeBlock className='bg-[#7147c6] text-white p-2'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">Web Design and Development</h2>
+                    <p>
+                        Proactively seize installed baseapplications whereas .
+                    </p>
                     <div className="card-actions justify-start">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="text-secondary font-bold">Read More</button>
                     </div>
                 </div>
             </div>
+
             <div className="card bg-base-100 shadow-xl w-60">
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <MdOutlineBusinessCenter className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">Business Developments</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
                     </div>
                 </div>
             </div>
+
             <div className="card bg-base-100 shadow-xl w-60">
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                    <FaProductHunt className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">Product Managements</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-xl w-60">
+                <div className="card-body">
+                    <SiAntdesign className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">UI/UX <br /> Designs</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-xl w-60">
+                <div className="card-body">
+                    <FaMapMarkerAlt className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">Marketing And Communications</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-xl w-60">
+                <div className="card-body">
+                    <IoShareSocialSharp className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">Social <br /> Marketing</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-xl w-60">
+                <div className="card-body">
+                    <BsSearch className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">SEO & <br /> Content Writing</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card bg-base-100 shadow-xl w-60">
+                <div className="card-body">
+                    <GrMonitor className='bg-[#E2D2FE] p-2 text-blue-900'
+                        size={50}
+                    />
+                    <h2 className="card-title font-bold">Digital <br />Marketing</h2>
+                    <p>
+                        Proactively seize installed base
+                        applications whereas .
+                    </p>
+                    <div className="card-actions justify-start">
+                        <button className="text-secondary font-bold">Read More</button>
                     </div>
                 </div>
             </div>
--- src/Pages/Servece-Pages/Banner/OurServices.js
+++ src/Pages/Servece-Pages/Banner/OurServices.js
@@ -4,15 +4,17 @@ import OurService from './OurService';
 const OurServices = () => {
     return (
         <>
-            <section className='text-center py-36'>
-                <div className='mx-auto bg-base-200 inline p-3 text-primary font-semibold'>
-                    What We Do
+            <section className='py-36'>
+                <div className='text-center'>
+                    <p className=' bg-base-200 inline p-3 text-primary font-semibold'>
+                        What We Do
+                    </p>
                 </div>
-                <h3 className='font-semibold text-4xl text-[#040741] my-4'>Our Services</h3>
+                <h3 className='font-semibold text-4xl text-[#040741] my-4 text-center'>Our Services</h3>
                 <p className='text-center md:px-44'>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam totam deserunt sapiente deleniti culpa laboriosam provident labore, molestiae dolorem possimus nostrum, sint consequuntur dicta aliquid sit praesentium? Ratione explicabo doloribus reiciendis animi, officia ullam voluptatem.
                 </p>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:px-36'>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 md:px-36 gap-y-3'>
                     <OurService />
                 </div>
             </section>
