e00363db8daf7c0a22f5cf535e4d80d2d8a475d6
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-01 18:41:28 +0600

    about page design started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import AboutUs from "./components/AboutUs";
 import Services from "./components/Services";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/services" element={<Services />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </div>
   );
---
+++ src/components/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AboutBanner from '../Pages/AboutUs-Page/AboutBanner';
+import Navbar from '../Pages/Navbar/Navbar';
+
+const AboutUs = () => {
+    return (
+        <>
+            <Navbar />
+            <AboutBanner />
+        </>
+    );
+};
+
+export default AboutUs;
