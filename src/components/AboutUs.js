f8af96c2381ec4a6d628b9628f994cd20ead1040
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-01 19:39:48 +0600

    footer done

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AboutBanner from '../Pages/AboutUs-Page/AboutBanner';
 import AboutSection from '../Pages/AboutUs-Page/AboutSection';
 import WhyChooseUs from '../Pages/AboutUs-Page/WhyChooseUs';
+import Footer from '../Pages/Footer/Footer';
 import Navbar from '../Pages/Navbar/Navbar';
 
 const AboutUs = () => {
@@ -11,6 +12,7 @@ const AboutUs = () => {
             <AboutBanner />
             <AboutSection />
             <WhyChooseUs />
+            <Footer />
         </>
     );
 };
