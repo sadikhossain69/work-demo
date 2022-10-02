ec3c211a789fb6ec3f6b8a72144d2feb8186712d
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-03 03:24:04 +0600

    package removed

--- src/Pages/AboutUs-Page/OurSkills.js
+++ src/Pages/AboutUs-Page/OurSkills.js
@@ -16,8 +16,7 @@ const OurSkills = () => {
                         <p className="mb-8 leading-relaxed">Enthusiastically customize emerging e-services whereas cross functional
                             users, Creaibly productivate diverse manufactured products</p>
                         <div className="flex justify-center">
-                            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+                            
                         </div>
                     </div>
                     <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
