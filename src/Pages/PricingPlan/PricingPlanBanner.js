b07af331af7fc10ac484a50ed168f23b9a3c4260
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-03 03:55:58 +0600

    banner done

--- src/Pages/PricingPlan/PricingPlanBanner.js
+++ src/Pages/PricingPlan/PricingPlanBanner.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import banner from '../../assets/output-onlinepngtools.png';
 
 const PricingPlanBanner = () => {
     return (
-        <div>
-            hello
+        <div className='md:px-36 md:py-16' style={{ backgroundImage: `url(${banner})`, backgroundSize: "cover", minHeight: '60vh' }}>
+            <div className='text-5xl font-bold text-white' >
+                Pricing Plans
+            </div>
+            <div className='text-white font-medium mt-3'>
+                Home {">"} Pricing Plans
+            </div>
         </div>
     );
 };
