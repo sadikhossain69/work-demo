69b9ef43c84bd27f83d476d26b9dcb26b3e3a22b
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-03 03:52:22 +0600

    pricing plan route created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
+import PricingPlans from "./components/PricingPlans";
 import Services from "./components/Services";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/pricingplan" element={<PricingPlans />} />
       </Routes>
     </div>
   );
---
+++ src/components/PricingPlans.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PricingPlanBanner from '../Pages/PricingPlan/PricingPlanBanner';
+
+const PricingPlans = () => {
+    return (
+        <>
+            <PricingPlanBanner />
+        </>
+    );
+};
+
+export default PricingPlans;
