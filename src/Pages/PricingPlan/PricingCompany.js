ce80dc44b46c8919291aff23c4a4a53b29fb142d
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-03 04:33:52 +0600

    pricing company done

---
+++ src/Pages/PricingPlan/PricingCompany.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const PricingCompany = () => {
+    return (
+        <>
+            <section className='my-10 text-center'>
+                <div className="stats shadow">
+
+                    <div className="stat">
+                        <img src="https://dummyimage.com/200x150" alt="" />
+                    </div>
+
+                    <div className="stat">
+                        <img src="https://dummyimage.com/200x150" alt="" />
+                    </div>
+
+                    <div className="stat">
+                        <img src="https://dummyimage.com/200x150" alt="" />
+                    </div>
+
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default PricingCompany;
--- src/components/PricingPlans.js
+++ src/components/PricingPlans.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../Pages/Footer/Footer';
 import Navbar from '../Pages/Navbar/Navbar';
 import PlanCategorys from '../Pages/PricingPlan/PlanCategorys';
+import PricingCompany from '../Pages/PricingPlan/PricingCompany';
 import PricingPlanBanner from '../Pages/PricingPlan/PricingPlanBanner';
 import Support from '../Pages/Servece-Pages/Support';
 
@@ -12,6 +13,7 @@ const PricingPlans = () => {
             <PricingPlanBanner />
             <PlanCategorys />
             <Support />
+            <PricingCompany/>
             <Footer />
         </>
     );
