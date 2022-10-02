d66220cf5e857c410f8f43e45db941234d7a9599
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-03 03:44:18 +0600

    get in touch section done

--- src/Pages/AboutUs-Page/GetInTouch.js
+++ src/Pages/AboutUs-Page/GetInTouch.js
@@ -4,8 +4,18 @@ const GetInTouch = () => {
     return (
         <>
             <section className='my-3'>
-                <div className='w-2/3 bg-gray-100'>
-                    hello
+                <div className='md:w-2/3 bg-gray-100 mx-auto flex justify-around py-12 flex-col md:flex-row'>
+                    <div className='space-y-1'>
+                        <p>
+                            We are 24/7 here to answer your questions
+                        </p>
+                        <div className='text-[#030642] font-bold text-4xl'>
+                            You need our help?
+                        </div>
+                    </div>
+                    <div>
+                        <button className='btn btn-primary rounded'>Get In Touch</button>
+                    </div>
                 </div>
             </section>
         </>
