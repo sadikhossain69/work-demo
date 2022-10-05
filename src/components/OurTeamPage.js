a8933abcb9e0a6da5309ec45597f43651b21b002
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-05 18:05:50 +0600

    our team banner created

--- src/components/OurTeamPage.js
+++ src/components/OurTeamPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import OurTeamBanner from '../Pages/OurTeamPage/OurTeamBanner';
 
 const OurTeamPage = () => {
     return (
         <div>
-            
+            <OurTeamBanner />
         </div>
     );
 };
