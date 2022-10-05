4122c95eeff3da2a9a0db63d13030ad203c7feec
sadikhossain69 <sadikhossain433@gmail.com>
2022-10-05 18:06:23 +0600

    navbar imported

--- src/components/OurTeamPage.js
+++ src/components/OurTeamPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../Pages/Navbar/Navbar';
 import OurTeamBanner from '../Pages/OurTeamPage/OurTeamBanner';
 
 const OurTeamPage = () => {
     return (
         <div>
+            <Navbar />
             <OurTeamBanner />
         </div>
     );
